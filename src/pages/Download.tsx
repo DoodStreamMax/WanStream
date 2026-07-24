fa1f38ed29cc1e5f1e3f4a314f936247a6131a84
DoodStreamMax <142033079+DoodStreamMax@users.noreply.github.com>
2026-07-25 02:36:56 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -4,7 +4,8 @@ export function Download() {
   const videoUrl = sessionStorage.getItem('videoUrl');
 
   const randomUrls = [
-    'https://crn77.com/4/10251220',
+    'https://dulyhagglermounting.com/2082665',
+    'https://sorrowfulpsychology.com/HE9TFh'
   ];
   
   const handleDownload = () => {
