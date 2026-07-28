7ced37000cba48f51813722b4a23454368c96802
DoodStreamMax <142033079+DoodStreamMax@users.noreply.github.com>
2026-07-29 03:15:08 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -5,7 +5,7 @@ export function Download() {
 
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://sorrowfulpsychology.com/HE9TFh'
+    'https://www.effectivecpmnetwork.com/b3c368xkxz?key=430d12c7fd58ecba841fd905ca2dd3fd'
   ];
   
   const handleDownload = () => {
