65a3f1cd67478c31a39e578d1ca97f60a8644389
DoodStreamMax <142033079+DoodStreamMax@users.noreply.github.com>
2026-08-10 02:22:47 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -5,7 +5,7 @@ export function Download() {
 
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://wwp.giriucat.com/redirect-zone/5137a848'
+    'https://sorrowfulpsychology.com/HE9TFh'
   ];
   
   const handleDownload = () => {
