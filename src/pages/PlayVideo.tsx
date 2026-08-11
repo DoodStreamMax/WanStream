1be2fdb69b4591ac4c22cc72354859d2eecdf1a8
DoodStreamMax <142033079+DoodStreamMax@users.noreply.github.com>
2026-08-12 00:48:30 +0500

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -95,7 +95,7 @@ export function PlayVideo() {
 
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://sorrowfulpsychology.com/HE9TFh'
+    'https://omg10.com/4/11556888'
   ];
 
   const shuffleArray = (array: any[]) => {
